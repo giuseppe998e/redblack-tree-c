13fdf1feeb4a6c94d42f432e97610bb7031d7c7e
Giuseppe Eletto <peppe.eletto@gmail.com>
2021-03-23 02:35:27 +0100

    Update header comments

--- src/rbtree.h
+++ src/rbtree.h
@@ -22,33 +22,37 @@
 #include <stddef.h>
 
 /**
- * RedBlack-Tree pointer
+ * RedBlack-Tree pointer.
  */
 typedef struct __rbtree *rbtree;
 
 /**
- * Initializes a new empty RedBlack-Tree
+ * Initializes a new empty RedBlack-Tree.
  */
 rbtree rbtree_init();
 
 /**
- * Looks for a node in the RedBlack-Tree
+ * Looks for a node in the RedBlack-Tree.
  */
 int rbtree_search(rbtree, unsigned);
 
 /**
  * Looks for a node in the RedBlack-Tree
- * and returns the pointer to its stored value
+ * and returns the pointer to its stored value.
  */
 void *rbtree_get(rbtree, unsigned);
 
 /**
- * Inserts a new node into the RedBlack-Tree
+ * Inserts a new node in the RedBlack-Tree.
+ * If size_t is equal to zero but not the void*,
+ * a reference to that pointer is saved, otherwise
+ * if size_t is NOT equal to zero the memory pointed
+ * will be cloned.
  */
 int rbtree_insert(rbtree, unsigned, void*, size_t);
 
 /**
- * Deletes a node from the RedBlack-Tree
+ * Deletes a node from the RedBlack-Tree.
  */
 void rbtree_delete(rbtree, unsigned); // TODO NOTIMPL
 
@@ -60,7 +64,7 @@ void rbtree_delete(rbtree, unsigned); // TODO NOTIMPL
 void *rbtree_remove(rbtree, unsigned); // TODO NOTIMPL
 
 /**
- * Frees a RedBlack-Tree
+ * Frees a RedBlack-Tree.
  */
 void rbtree_free(rbtree);
 
